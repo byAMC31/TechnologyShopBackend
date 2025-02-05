9c3744da3f79bae34c34786b6ff900294d570284
Adrian Martinez Calderon <adrianmtzcl@gmail.com>
2025-02-05 17:45:08 -0600

    fix: correcciones en la creacion e insercion en la BD

--- BD/script_creacion.sql
+++ BD/script_creacion.sql
@@ -5,59 +5,64 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema tecnologyshop
+-- Schema technologyshop
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema tecnologyshop
+-- Schema technologyshop
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `tecnologyshop` DEFAULT CHARACTER SET utf8 ;
-USE `tecnologyshop` ;
+CREATE SCHEMA IF NOT EXISTS `technologyshop` DEFAULT CHARACTER SET utf8 ;
+USE `technologyshop` ;
 
 -- -----------------------------------------------------
--- Table `tecnologyshop`.`usuarios`
+-- Table `technologyshop`.`usuarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tecnologyshop`.`usuarios` (
-  `id_usuario` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `technologyshop`.`usuarios` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(60) NOT NULL,
   `telefono` VARCHAR(10) NOT NULL,
   `email` VARCHAR(60) NOT NULL,
-  `contraseña` VARCHAR(60) NOT NULL,
+  `password` VARCHAR(60) NOT NULL,
   `tipo` VARCHAR(15) NOT NULL,
-  PRIMARY KEY (`id_usuario`),
-  UNIQUE INDEX `id_usuario_UNIQUE` (`id_usuario` ASC))
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_usuario_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `tecnologyshop`.`productos`
+-- Table `technologyshop`.`productos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tecnologyshop`.`productos` (
-  `id_producto` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `technologyshop`.`productos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(60) NOT NULL,
-  `descripcion` VARCHAR(300) NOT NULL,
-  `imagen` VARCHAR(30) NOT NULL,
-  `precio` FLOAT(7,2) NOT NULL,
+  `descripcion` VARCHAR(800) NOT NULL,
+  `imagen` VARCHAR(500) NOT NULL,
+  `precio` DECIMAL(10,2) NOT NULL,
   `categoria` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_producto`),
-  UNIQUE INDEX `id_producto_UNIQUE` (`id_producto` ASC))
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_producto_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `tecnologyshop`.`compras`
+-- Table `technologyshop`.`compras`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tecnologyshop`.`compras` (
-  `id_compra` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id_compra`),
-  UNIQUE INDEX `id_compra_UNIQUE` (`id_compra` ASC))
+CREATE TABLE IF NOT EXISTS `technologyshop`.`compras` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `total` DECIMAL(10,2) NOT NULL,
+  `fecha` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_compra_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `tecnologyshop`.`detalle_de_compra`
+-- Table `technologyshop`.`detalle_de_compra`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tecnologyshop`.`detalle_de_compra`
+CREATE TABLE IF NOT EXISTS `technologyshop`.`detalle_de_compra` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
